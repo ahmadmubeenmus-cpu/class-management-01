646a5cbb9bda4b3d5bdcf961bb74c901c79c313d
Mubeen Ahmad <ahmadmubeen.mus@gmail.com>
2026-01-08 19:09:45 +0000

    pages should be on rigt sight startfrom profile side in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -50,34 +50,36 @@ export function Header() {
                 <GraduationCap className="h-6 w-6 text-primary" />
                 <span className="font-bold">Class Managment</span>
             </Link>
-            <SidebarNav isCollapsed={false} />
          </div>
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                 <Button variant="ghost" className="relative h-10 w-10 rounded-full">
-                    <Avatar>
-                        <AvatarImage src={userProfile?.profilePictureUrl} alt={userProfile?.firstName} />
-                        <AvatarFallback>{getInitials(userProfile?.firstName, userProfile?.lastName)}</AvatarFallback>
-                    </Avatar>
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-56" align="end" forceMount>
-                <DropdownMenuLabel className="font-normal">
-                    <div className="flex flex-col space-y-1">
-                        <p className="text-sm font-medium leading-none">{userProfile?.firstName} {userProfile?.lastName}</p>
-                        <p className="text-xs leading-none text-muted-foreground">{userProfile?.email}</p>
-                    </div>
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem onClick={() => router.push('/profile')}>
-                    Profile
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={handleLogout}>
-                    <LogOut className="mr-2 h-4 w-4" />
-                    <span>Log out</span>
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <div className="flex items-center gap-4">
+            <SidebarNav isCollapsed={false} />
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" className="relative h-10 w-10 rounded-full">
+                        <Avatar>
+                            <AvatarImage src={userProfile?.profilePictureUrl} alt={userProfile?.firstName} />
+                            <AvatarFallback>{getInitials(userProfile?.firstName, userProfile?.lastName)}</AvatarFallback>
+                        </Avatar>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56" align="end" forceMount>
+                    <DropdownMenuLabel className="font-normal">
+                        <div className="flex flex-col space-y-1">
+                            <p className="text-sm font-medium leading-none">{userProfile?.firstName} {userProfile?.lastName}</p>
+                            <p className="text-xs leading-none text-muted-foreground">{userProfile?.email}</p>
+                        </div>
+                    </DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem onClick={() => router.push('/profile')}>
+                        Profile
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={handleLogout}>
+                        <LogOut className="mr-2 h-4 w-4" />
+                        <span>Log out</span>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </div>
 
       </nav>
        <Sheet open={isMobileMenuOpen} onOpenChange={setMobileMenuOpen}>
