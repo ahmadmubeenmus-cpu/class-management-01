d97dfa089de7409fa500cc5ecec0eda232dc4ed8
Mubeen Ahmad <ahmadmubeen.mus@gmail.com>
2026-01-08 13:23:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4770832997-3654c",
+  "appId": "1:549994663668:web:6637d82076a4eb62f798a1",
+  "apiKey": "AIzaSyCjs2qpoPayCcduzGOUfmHWbjln2RoV-xE",
+  "authDomain": "studio-4770832997-3654c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "549994663668"
+};
